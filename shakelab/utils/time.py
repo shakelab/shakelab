9230be6ffa5c8b66bdb90ee27d13ac07a8786fc3
klunk386 <poggi.valerio@gmail.com>
2019-10-15 17:56:19 +0200

    fixed date class issue

--- shakelab/utils/time.py
+++ shakelab/utils/time.py
@@ -18,60 +18,159 @@
 #
 # ============================================================================
 """
+Time and date functionalities
 """
 
-DSEC = 24.*3600.
-YDAYS = 365.
+from datetime import datetime
 
 class Date(object):
     """
     """
 
-    year = 1
-    month = 1
-    day = 1
-    hour = 0
-    minute = 0
-    second = 0.0
+    def __init__(self, date=None):
+        if date is not None:
+            self.set(date)
 
-    def __init__(self, date_list=None):
-        if date_list is not None:
-            self.set(date_list)
-
-    def set(self, date_list):
-        self.year = date_list[0]
-        self.month = date_list[1]
-        self.day = date_list[2]
-        self.hour = date_list[3]
-        self.minute = date_list[4]
-        self.second = date_list[5]
+    def set(self, date):
+        """
+        """
+        self.year = int(date[0])
+        self.month = int(date[1])
+        self.day = int(date[2])
+        self.hour = int(date[3])
+        self.minute = int(date[4])
+        self.second = float(date[5])
 
     def get(self):
+        """
+        """
         return [self.year, self.month, self.day,
                 self.hour, self.minute, self.second]
 
-    def to_sec(self):
+    def to_second(self):
+        """
+        """
         return date_to_sec(self.year, self.month, self.day,
                            self.hour, self.minute, self.second)
 
-    def shift_time(self):
-        print('to_implement')
+    def from_second(self, second):
+        """
+        """
+        self.set(sec_to_date(second))
 
-    def __add__(self, date):
-        if isinstance(date, (int, float)):
-            return self.to_sec() + date
-        if isinstance(date, Date):
-            return self.to_sec() + date.to_sec()
+    def shift_time(self, second, unit='s'):
+        """
+        """
+        if unit is 'm':
+            second *= 60
+        if unit is 'h':
+            second *= 3600
+        if unit is 'd':
+            second *= 86400
 
-    def __sub__(self, date):
-        if isinstance(date, (int, float)):
-            return self.to_sec() - date
-        if isinstance(date, Date):
-            return self.to_sec() - date.to_sec()
+        self.from_second(self.to_second() + second)
 
-# TO IMPLEMENT
-def sec_to_date(second):
+    def __add__(self, value):
+        """
+        """
+        if isinstance(value, (int, float)):
+            dnew = Date()
+            dnew.from_second(self.to_second() + value)
+            return dnew
+        else:
+            print('Not a supported operation')
+
+    def __sub__(self, value):
+        """
+        """
+        if isinstance(value, (int, float)):
+            dnew = Date()
+            dnew.from_second(self.to_second() - value)
+            return dnew
+        elif isinstance(value, Date):
+            return self.to_second() - value.to_second()
+        else:
+            print('Not a supported operation')
+
+    def __repr__(self):
+        """
+        """
+        return repr(self.get())
+
+
+def date_to_sec(year, month, day, hour, minute, second):
     """
+    """
+    isecond = int(second // 1)
+    msecond = int((second % 1) * 1e6)
+
+    t0 = datetime(1 ,1 ,1 ,0 ,0 ,0 ,0)
+    t1 = datetime(year, month, day, hour, minute, isecond, msecond)
+    return (t1 - t0).total_seconds()
+
+def sec_to_date(second=0.):
+    """
+    NOTE: Datetime produces rounding errors for unknown reason.
+    """
+    sref = date_to_sec(1970, 1, 1, 0, 0, 0.)
+    d = datetime.utcfromtimestamp(second - sref)
+    fsecond = d.second + (d.microsecond * 1e-6)
+    return [d.year, d.month, d.day, d.hour, d.minute, fsecond]
+
+# -----------------------------------------------------------------------------
+# MY IMPLEMENTATION - UNDER DEVELOPMENT
+
+DSEC = 24.*3600.
+YDAYS = 365.
+
+def leap_check(year):
+    """
+    Check if leap year.
+    """
+
+    c0 = (year % 4 == 0)
+    c1 = (year % 100 != 0)
+    c2 = (year % 400 == 0)
+
+    return (c0 and c1) or c2
+
+def leap_num(year):
+    """
+    """
+
+    n0 = (year-1)//4
+    n1 = (year-1)//100
+    n2 = (year-1)//400
+
+    return n0 - n1 + n2
+
+def date_to_sec_mine(year=1, month=1, day=1, hour=0, minute=0, second=0.0):
+    """
+    Convert a date to seconds.
+    Not yet implemented for b.c. years.
+    """
+
+    if year < 1:
+      print('Error: Year must be positive (> 1)')
+      return None
+
+    if leap_check(year):
+        MDAYS = [0.,31.,60.,91.,121.,152.,182.,213.,244.,274.,305.,335.]
+    else:
+        MDAYS = [0.,31.,59.,90.,120.,151.,181.,212.,243.,273.,304.,334.]
+
+    ysec = (year - 1) * YDAYS * DSEC
+    ysec += leap_num(year) * DSEC
+    msec = MDAYS[int(month) - 1] * DSEC
+    dsec = (day - 1) * DSEC
+
+    sec = ysec + msec + dsec + hour*3600.+ minute*60. + second*1.
+
+    return sec
+
+def sec_to_date_mine(second):
+    """
+    IN PROGRESS!!!
     """
     # Number of days in 4 years (including a leap year)
     ysec = YDAYS * DSEC
@@ -107,47 +206,3 @@ def sec_to_date(second):
 
     print(year, month, sres)
 
-def date_to_sec(year=1, month=1, day=1, hour=0, minute=0, second=0.0):
-    """
-    Convert a date to seconds.
-    Not yet implemented for b.c. years.
-    """
-
-    if year < 1:
-      print('Error: Year must be > 1')
-      return None
-
-    if leap_check(year):
-        MDAYS = [0.,31.,60.,91.,121.,152.,182.,213.,244.,274.,305.,335.]
-    else:
-        MDAYS = [0.,31.,59.,90.,120.,151.,181.,212.,243.,273.,304.,334.]
-
-    ysec = (year - 1) * YDAYS * DSEC
-    ysec += leap_num(year) * DSEC
-    msec = MDAYS[int(month) - 1] * DSEC
-    dsec = (day - 1) * DSEC
-
-    sec = ysec + msec + dsec + hour*3600.+ minute*60. + second*1.
-
-    return sec
-
-def leap_check(year):
-    """
-    Check if leap year.
-    """
-
-    c0 = (year % 4 == 0)
-    c1 = (year % 100 != 0)
-    c2 = (year % 400 == 0)
-
-    return (c0 and c1) or c2
-
-def leap_num(year):
-    """
-    """
-
-    n0 = (year-1)//4
-    n1 = (year-1)//100
-    n2 = (year-1)//400
-
-    return n0 - n1 + n2
