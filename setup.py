c5745b93b61826f880d4525f215d1b842f7b3210
klunk386 <poggi.valerio@gmail.com>
2019-11-15 09:00:32 +0100

    modified setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='quakelab',
-    version='0.0.1',
+    name='shakelab',
+    version='2019.11.dev1',
     author='Valerio Poggi',
     author_email='vpoggi@inogs.it',
     description='Python tools for engineering seismology',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/klunk386/QuakeLab',
+    url='https://github.com/shakelab/shakelab',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -20,6 +20,7 @@ setuptools.setup(
         'Topic :: Scientific/Engineering',
         'Intended Audience :: Science/Research'
     ],
+    python_requires='>=3.6',
     install_requires=[
         'setuptools',
         'numpy',
